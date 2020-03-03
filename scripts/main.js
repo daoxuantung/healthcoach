df5223bc8737b1212af78af99daab0b056ec7128
leooblivion <leooblivion24@gmail.com>
2020-03-04 02:46:52 +0800

    Fix animation images

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,31 @@
-var slideIndex = 0;
-// clickSlides();
+//khai báo biến slideIndex đại diện cho slide hiện tại
+var slideIndex;
+//mặc định hiển thị slide đầu tiên 
+showSlides(slideIndex = 0);
 
-window.onload = function showSlides() {
-    
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+// KHai bào hàm hiển thị slide
+function showSlides() {
+    var i;
     var slides = document.getElementsByClassName("slide");
     var buttons = document.getElementsByClassName("button");
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].className =buttons[i].className.replace(" button-actived", "");
     }
 
+    slides[slideIndex].style.display = "block";  
+    buttons[slideIndex].className += " button-actived";
+    //chuyển đến slide tiếp theo
     slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].className = buttons[i].className.replace(" button-actived", "");
-    }
-
-    slides[slideIndex-1].style.display = "block";  
-    buttons[slideIndex-1].className += " button-actived";
-    setTimeout(showSlides, 4000);
+    //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+    if (slideIndex > slides.length - 1) {
+        slideIndex = 0;
+    }    
+    //tự động chuyển đổi slide sau 4s
+    setTimeout(showSlides, 5000);
 }
